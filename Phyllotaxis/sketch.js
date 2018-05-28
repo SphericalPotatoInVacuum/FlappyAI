b47fe87c904066c3ba522b1b1a66ceabffe75d18
SphericalPotatoInVacuum <a@bogdanov.co>
2018-05-28 19:22:57 +0300

    Last Phyllotaxis commit (I hope)

--- Phyllotaxis/sketch.js
+++ Phyllotaxis/sketch.js
@@ -1,5 +1,15 @@
-var n = 6000;
+var nSlider;
+var nText;
+var n = 0;
 var cSlider;
+var cText;
+var c = 0;
+var aSlider;
+var aText;
+var angle = 0;
+var sSlider;
+var sText;
+var s = 0;
 var updateBtn;
 
 function setup() {
@@ -10,24 +20,41 @@ function setup() {
     noStroke();
     updateBtn = createButton("Update").addClass("update");
     updateBtn.mouseClicked(update);
-    cSlider = createSlider(0, 50, 12, 1).addClass("cSlider");
+    cSlider = createSlider(0, 50, 10, 1).addClass("cSlider");
+    cText = createP().addClass("cText");
+    nSlider = createSlider(0, 6000, 2000, 100).addClass("nSlider");
+    nText = createP().addClass("nText");
+    aSlider = createSlider(137, 138, 137.78, 0.02).addClass("aSlider");
+    aText = createP().addClass("aText");
+    sSlider = createSlider(1, 50, 32, 1).addClass("sSlider");
+    sText = createP().addClass("sText");
     update();
 }
 
 function draw() {
-
+    if (c != cSlider.value() | n != nSlider.value() | angle != aSlider.value() | s != sSlider.value()) {
+        update();
+        console.log("FUCK");
+    }
 }
 
 function update() {
     background(0);
-    let c = cSlider.value();
     translate(width / 2, height / 2);
+    c = cSlider.value();
+    cText.html("Radius coeff.: " + c);
+    n = nSlider.value();
+    nText.html("Total count: " + n);
+    angle = aSlider.value();
+    aText.html("Angle delta: " + angle);
+    s = sSlider.value();
+    sText.html("Dot size: " + s);
     for (let i = 0; i < n; i++) {
-        let angle = 137.5 * i;
+        let a = angle * i;
         let r = c * Math.sqrt(i);
-        let x = r * sin(angle);
-        let y = r * cos(angle);
-        fill(r * 2 % 350, 255, 255);
-        ellipse(x, y, 40);
+        let x = r * sin(a);
+        let y = r * cos(a);
+        fill(-(r - a / 2) % 360, 255, 255);
+        ellipse(x, y, s);
     }
 }
